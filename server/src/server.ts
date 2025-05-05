0db24cb735bd8769555020bcc5f757c7a5e98316
Gurtej Malik <gurtejmalik@gmail.com>
2025-05-04 20:54:04 -0700

    Docker compose causing issue, begin dockerless dev in new branch

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
 
